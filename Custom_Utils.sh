2fd2370bb67b01897b3363e889a79e03a73481db
Nagatsu <nagatsu.v@gmail.com>
2023-09-02 18:13:57 +0200

    modified:   Custom_Utils.sh

--- Custom_Utils.sh
+++ Custom_Utils.sh
@@ -1,21 +1,31 @@
-#! /bin/bash
+#!/bin/bash
 # Il faut commencer par installer les 4 polices d'ecriture MesloLGS NF et les utiliser dans notre terminal (Tabby) 
 # https://github.com/IlanCosman/tide#fonts
-#
-# Ajout du repository pour installer fish 
+
+# Ajout du repository pour installer fish
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt -y update && sudo apt -y upgrade
-# Installation de bat une alternative a cat qui implemente la couleur sytaxique et les numero de ligne
+
+# Installation de bat
 sudo apt -y install bat
-# Installation de micro un editeur de text mieux que nano et il supporte la souris
-sudo apy -y install micro
-# Installation de exa un alternative a ls je vais creer les alias plus loins
+
+# Installation de micro
+sudo apt -y install micro
+
+# Installation de exa
 sudo apt -y install exa
-# Installation de fish ! un super remplacant de bash 
-sudo apt install fish
-# Intalaltion de fisher un gestionnaire de plugin pour fish
+
+# Installation de fish
+sudo apt -y install fish
+
+# Installation de fisher (gestionnaire de plugins)
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-# Installation du theme Tide 
+
+# Installation du theme Tide
 fisher install IlanCosman/tide@v5
+
 # Mise en place de Fish comme shell par defaut
-chsh -s /usr/bin/fish 
+chsh -s /usr/bin/fish
+
+# Recapitulatif
+echo "Installation terminee."
