8e76d8ca4607914c11aa304d9d156fbfd12cc681
Nagatsu <nagatsu.v@gmail.com>
2023-09-02 18:21:30 +0200

    modified:   Custom_Utils.sh     Ajouts de texte indiquant chaque etapes du script     Retrait du texte de sortie des commandes

--- Custom_Utils.sh
+++ Custom_Utils.sh
@@ -4,36 +4,49 @@ echo "https://github.com/IlanCosman/tide#fonts"
 
 sleep 5
 
-# Verification des droits d'administration
-if [ "$EUID" -ne 0 ]
-  then echo "Ce script doit etre execute en tant qu'administrateur (avec sudo)." 
+# Vérifions que nous avons les droits d'administration
+if [ "$EUID" -ne 0 ]; then
+  echo "Ce script doit être exécuté en tant qu'administrateur (avec sudo)."
   exit
 fi
 
-# Ajout du repository pour installer fish
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt -y update && sudo apt -y upgrade
+# Ajout du repository pour installer fish et mise a jour
+echo "Ajout du repository pour installer fish et mise a jour."
+sudo apt-add-repository ppa:fish-shell/release-3 >dev/null
+sudo apt -y update && sudo apt -y upgrade >dev/null
 
 # Installation de bat
-sudo apt -y install bat
+echo "Installation de bat."
+sudo apt -y install bat >dev/null
 
 # Installation de micro
-sudo apt -y install micro
+echo "Installation de micro."
+sudo apt -y install micro >dev/null
 
 # Installation de exa
-sudo apt -y install exa
+echo "Installation de exa."
+sudo apt -y install exa >dev/null
 
 # Installation de fish
-sudo apt -y install fish
+echo "Installation de fish."
+sudo apt -y install fish  >dev/null
+
+# Lancement de fish
+echo "Lancement de fish."
+fish
 
 # Installation de fisher (gestionnaire de plugins)
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+echo "Installation de fisher."
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher >dev/null
 
 # Installation du theme Tide
-fisher install IlanCosman/tide@v5
+echo "Installation du theme Tide."
+fisher install IlanCosman/tide@v5 >dev/null
 
 # Mise en place de Fish comme shell par defaut
-chsh -s /usr/bin/fish
+echo "Mise en place de Fish comme shell par defaut."
+chsh -s /usr/bin/fish >dev/null
 
 # Recapitulatif
-echo "Installation terminee."
+echo "Installation terminée."
+sleep 5
