0f5ae77e2dc7b7546b7fd015673d6319eb2d3a3a
Nagatsu <nagatsu.v@gmail.com>
2023-09-02 18:34:48 +0200

    modified:   >dev/null to >/dev/null

--- Custom_Utils.sh
+++ Custom_Utils.sh
@@ -1,35 +1,69 @@
 #!/bin/bash
+# Ce script installe les outils et les thèmes suivants :
+# - bat
+# - micro
+# - exa
+# - fish
+# - fisher
+# - tide
+
 echo "Il faut commencer par installer les 4 polices d'ecriture MesloLGS NF et les utiliser dans notre terminal (Tabby)"
 echo "https://github.com/IlanCosman/tide#fonts"
 
+#pause de 5 secondes
 sleep 5
 
-# Vérifions que nous avons les droits d'administration
-if [ "$EUID" -ne 0 ]; then
-  echo "Ce script doit être exécuté en tant qu'administrateur (avec sudo)."
+# Verification des droits d'administration
+if [ "$EUID" -ne 0 ]
+  then echo "Ce script doit etre execute en tant qu'administrateur (avec sudo)." 
   exit
 fi
 
 # Ajout du repository pour installer fish et mise a jour
 echo "Ajout du repository pour installer fish et mise a jour."
-sudo apt-add-repository ppa:fish-shell/release-3 >dev/null
-sudo apt -y update && sudo apt -y upgrade >dev/null
+sudo apt-add-repository ppa:fish-shell/release-3 >/dev/null
+if [ $? -eq 0 ]; then
+  echo "Ajout du repository reussie."
+else
+  echo "Ajout du repository echouee."
+fi
+sudo apt -y update && sudo apt -y upgrade >/dev/null
+if [ $? -eq 0 ]; then
+  echo "mise a jour reussie."
+else
+  echo "mise a jour echouee."
+fi
 
 # Installation de bat
 echo "Installation de bat."
-sudo apt -y install bat >dev/null
+sudo apt -y install bat >/dev/null
+if [ $? -eq 0 ]; then
+  echo "Installation de bat reussie."
+else
+  echo "Installation de bat echouee."
+fi
 
 # Installation de micro
 echo "Installation de micro."
-sudo apt -y install micro >dev/null
+sudo apt -y install micro >/dev/null
+if [ $? -eq 0 ]; then
+  echo "Installation de micro reussie."
+else
+  echo "Installation de micro echouee."
+fi
 
 # Installation de exa
 echo "Installation de exa."
-sudo apt -y install exa >dev/null
+sudo apt -y install exa >/dev/null
 
 # Installation de fish
 echo "Installation de fish."
-sudo apt -y install fish  >dev/null
+sudo apt -y install fish  >/dev/null
+if [ $? -eq 0 ]; then
+  echo "Installation de exa reussie."
+else
+  echo "Installation de exa echouee."
+fi
 
 # Lancement de fish
 echo "Lancement de fish."
@@ -37,16 +71,31 @@ fish
 
 # Installation de fisher (gestionnaire de plugins)
 echo "Installation de fisher."
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher >dev/null
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher >/dev/null
+if [ $? -eq 0 ]; then
+  echo "Installation de fisher reussie."
+else
+  echo "Installation de fisher echouee."
+fi
 
 # Installation du theme Tide
 echo "Installation du theme Tide."
-fisher install IlanCosman/tide@v5 >dev/null
+fisher install IlanCosman/tide@v5 >/dev/null
+if [ $? -eq 0 ]; then
+  echo "Installation du theme Tide reussi."
+else
+  echo "Installation du theme Tide echouee."
+fi
 
 # Mise en place de Fish comme shell par defaut
 echo "Mise en place de Fish comme shell par defaut."
-chsh -s /usr/bin/fish >dev/null
+chsh -s /usr/bin/fish >/dev/null
+if [ $? -eq 0 ]; then
+  echo "Mise en place de Fish comme shell par defaut a reussie."
+else
+  echo "Mise en place de Fish comme shell par defaut a echouee."
+fi
 
 # Recapitulatif
-echo "Installation terminée."
+echo "Installation terminee."
 sleep 5
