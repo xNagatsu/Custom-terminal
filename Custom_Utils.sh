140731b314c5e28bd4d8a5b1de2189836c965b17
Nagatsu <nagatsu.v@gmail.com>
2023-09-03 01:36:43 +0200

    modified:   Custom_Utils.sh

--- Custom_Utils.sh
+++ Custom_Utils.sh
@@ -12,14 +12,10 @@ sleep 2
 sudo add-apt-repository ppa:fish-shell/release-3
 
 echo
-
 echo "Il faut installer les 4 polices d'ecriture MesloLGS NF et les utiliser dans notre terminal (Tabby) pour avoir les logos & cie"
 sleep 1.5
 echo "https://github.com/IlanCosman/tide#fonts"
-
 echo
-
-#pause de 3 secondes
 sleep 3
 
 # Verification des droits d'administration
@@ -40,7 +36,7 @@ sleep 1
 # Installation de bat
 echo
 echo "Installation de bat."
-sudo apt-get -y install --assume-yes bat >/dev/null
+sudo apt-get -y install bat >/dev/null
 if [ $? -eq 0 ]; then
   echo "Installation de bat reussie !"
 else
@@ -51,7 +47,7 @@ sleep 1
 # Installation de micro
 echo
 echo "Installation de micro."
-sudo apt-get -y install --assume-yes micro >/dev/null
+sudo apt-get -y install micro >/dev/null
 if [ $? -eq 0 ]; then
   echo "Installation de micro reussie !"
 else
@@ -62,7 +58,7 @@ sleep 1
 # Installation de exa
 echo
 echo "Installation de exa."
-sudo apt-get -y install --assume-yes exa >/dev/null
+sudo apt-get -y install exa >/dev/null
 if [ $? -eq 0 ]; then
   echo "Installation de exa reussie !"
 else
@@ -74,13 +70,15 @@ sleep 1
 # Installation de fish
 echo
 echo "Installation de fish."
-sudo apt-get -y install --assume-yes fish  >/dev/null
+sudo apt-get -y install fish
 if [ $? -eq 0 ]; then
   echo "Installation de fish reussie !"
 else
   echo -e "\033[31mInstallation de fish echouee !\033[0m"
 fi
 sleep 2
+
+
 
 # Generation du fichier custom_fish.sh
 echo
@@ -89,40 +87,46 @@ echo "Generation du fichier custom_fish.sh"
 sleep 2
 
 cat << EOF > ./custom_fish.sh
-
+#!/usr/bin/fish
 # Installation de fisher (gestionnaire de plugins)
 echo "Installation de fisher."
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher >/dev/null
-  echo "Installation de fisher reussie !"
-else
-  echo -e "\033[31mInstallation de fisher echouee !\033[0m"
-fi
 sleep 1
+if curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+     echo "Installation de fisher reussie !"
+else 
+    set_color red; echo "Installation de fisher echouee !"
+end
+    set_color normal; echo ""
+
+sleep 2
 
 # Installation du theme Tide
-echo "Installation du theme Tide."
-fisher install IlanCosman/tide@v5 
-if [ $? -eq 0 ]; then
-  echo "Installation du theme Tide reussi."
-else
-  echo -e "\033[31mInstallation du theme Tide echouee.\033[0m"
-fi
+    echo "Installation du theme Tide."
 sleep 1
+if fisher install IlanCosman/tide@v5 
+    echo "Installation du theme Tide reussi !"
+else
+    set_color red; echo "Installation du theme Tide echouee !"
+end
+    set_color normal; echo ""
+
+sleep 2
+
+tide configure
 
 # Mise en place de Fish comme shell par defaut
 echo "Mise en place de Fish comme shell par defaut."
-chsh -s /usr/bin/fish >/dev/null
-if [ $? -eq 0 ]; then
-  echo "Mise en place de Fish comme shell par defaut a reussie."
+if chsh -s /usr/bin/fish
+    echo "Mise en place de Fish comme shell par defaut a reussie !"
 else
-  echo -e "\033[31mMise en place de Fish comme shell par defaut a echouee.\033[0m"
-fi
-sleep 1
+    set_color red; echo "Mise en place de Fish comme shell par defaut a echouee !"
+end
+    set_color normal; echo ""
+sleep 2
 
 # Recapitulatif
 echo "Installation terminee."
-sleep 5
-
+sleep 1
 EOF
 
 chmod +x ./custom_fish.sh
@@ -138,4 +142,4 @@ sleep 3
 
 # Lancement de fish
 echo "Lancement de fish."
-sudo -u vince exec fish
+sudo -u $(basename $(pwd)) fish
