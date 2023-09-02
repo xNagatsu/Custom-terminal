9e8d6cff5cff327c642cf6ee6ea74e0f5c38a52a
Nagatsu <nagatsu.v@gmail.com>
2023-09-02 18:15:26 +0200

    modified:   Custom_Utils.sh

--- Custom_Utils.sh
+++ Custom_Utils.sh
@@ -2,6 +2,12 @@
 # Il faut commencer par installer les 4 polices d'ecriture MesloLGS NF et les utiliser dans notre terminal (Tabby) 
 # https://github.com/IlanCosman/tide#fonts
 
+# Verification des droits d'administration
+if [ "$EUID" -ne 0 ]
+  then echo "Ce script doit etre execute en tant qu'administrateur (avec sudo)." 
+  exit
+fi
+
 # Ajout du repository pour installer fish
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt -y update && sudo apt -y upgrade
