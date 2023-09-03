34696c727d68e4ebbe8ee74fbe51192f619fcb7f
Nagatsu <nagatsu.v@gmail.com>
2023-09-03 13:55:56 +0200

    modification pour les alias

--- Custom_Utils.sh
+++ Custom_Utils.sh
@@ -12,9 +12,9 @@ sleep 2
 sudo add-apt-repository ppa:fish-shell/release-3
 
 echo
-echo "Il faut installer les 4 polices d'ecriture MesloLGS NF et les utiliser dans notre terminal (Tabby) pour avoir les logos & cie"
+echo "\033[31mIl faut installer les 4 polices d'ecriture MesloLGS NF et les utiliser dans notre terminal (Tabby) pour avoir les logos & cie\033[0m"
 sleep 1.5
-echo "https://github.com/IlanCosman/tide#fonts"
+echo "\033[31mhttps://github.com/IlanCosman/tide#fonts\033[0m"
 echo
 sleep 3
 echo
@@ -122,24 +122,92 @@ tide configure
 
 echo "Creation des alias"
 
-### EXA
+################################# EXA #################################
 
-alias ls="exa -a --icons"                   # short, multi-line
-alias ll="exa -1a --icons"                  # list, 1 per line
-alias ld="ll"                               # ^^^, NOTE: Trying to move to this for alternate hand commands
-alias la="exa -lagh --icons"                # list with info
-alias lt="exa -a --tree --icons --level=2"  # list with tree level 2
-alias ltf="exa -a --tree --icons"           # list with tree
-alias lat="exa -lagh --tree --icons"        # list with info and tree
+function ls --description=""                    # Alias de la commande
+    command exa -a --icons                      # ce que l'alias doit executer // command permet de rendre la fonction plus robuste car il garantit que "exa' est traité comme une commande externe
+end                                             # Indique la fin de la fonction (alias)
+funcsave ls                                     # Va enregistre la fonction ls en créant le fichier ~/.config/fish/functions/ls.fish pour que l'alias soit dispo au redémarrage ce qui le rend permanent
 
-### BAT
 
-alias cat='batcat --style="header" --pager "less -RF"'
-alias catn='batcat --pager "less -RF"'
-alias batn='batcat --pager "less -RF"'
+function ll --description="list un par ligne"
+    command exa -1a --icons
+end
+funcsave ll
 
-### RACCOURCIS
-alias nano="micro" # Question d'habitudes ...
+
+function ld --description="Comme ll"
+    command ll
+end
+funcsave ld
+
+
+function la --description="Liste avec info"
+    command exa -lagh --icons
+end
+funcsave la
+
+
+function lt --description="liste en arborescence level 2"
+    command exa -a --tree --icons --level=2
+end
+funcsave lt
+
+
+function ltf --description="Liste en arborescence"
+    command exa -a --tree --icons
+end
+funcsave ltf
+
+
+function lat --description="liste en arborescence avec info"
+    command exa -lagh --tree --icons
+end
+funcsave lat
+
+
+################################# BAT #################################
+
+function cat --description=""
+    command batcat --style="header" --pager "less -RF"
+end
+funcsave cat
+
+
+function catn --description=""
+    command batcat --pager "less -RF"
+end
+funcsave catn
+
+
+function batn --description=""
+    command batcat --pager "less -RF"
+end
+funcsave batn
+
+################################# RACCOURCIS #################################
+
+function nano --description="Remplace nano par micro"
+    command micro
+end
+funcsave nano
+
+
+function ipa --description="ip -c a"
+    command ip -c a
+end
+funcsave ipa
+
+
+function ipr --description="ip -c route"
+    command ip -c route
+end
+funcsave ipr
+
+function ipn --description="ip -c neigh"
+    command ip -c neigh
+end
+funcsave ipn
 
 
 # Recapitulatif
